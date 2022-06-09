cfd8d13b097230f6aa2b23b844fa061989153bc7
syseoz <syseoz@naver.com>
2022-06-09 17:38:29 +0900

    add post main page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+
+from . models import Post
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
