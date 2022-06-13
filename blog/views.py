50301cee9984e216dec7c383372fbc0c995f464f
syseoz <syseoz@naver.com>
2022-06-13 16:34:07 +0900

    add create post

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-from . models import Post, Category
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from . models import Post, Category, Tag
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
+
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'hook_text', 'head_image', 'file_upload', 'category']
+
+    #form valid함수 재정의 , author필드 자동으로 채우기
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog')
 
 class PostList(ListView):
     model = Post
@@ -32,6 +46,9 @@ class PostDetail(DetailView):
         return context
 
 
+
+
+
 def category_page(request, slug):
     if slug == 'no_category':
         category = '미분류'
@@ -48,5 +65,24 @@ def category_page(request, slug):
             'categories' : Category.objects.all(),
             'no_category_post_count' : Post.objects.filter(category=None).count(),
             'category' : category,
+        }
+    )
+
+
+
+def tag_page(request, slug):
+
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
+            'category' : Category.objects.all(),
+            'tag':tag,
         }
     )
