f54fcff65b7e019832ac3924716f6b94bd4891d3
syseoz <syseoz@naver.com>
2022-06-16 23:04:11 +0900

    dockerFile

--- django_prj/urls.py
+++ django_prj/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('blog.urls')),
     #path('', include('single_pages.urls')),
 ]
